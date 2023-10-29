70ee96c544028efc78c2de24a442b681b605459b
tiger <tiger@qq.com>
2023-10-30 02:07:30 +0800

    fix

--- lib/spi/data_service.ts
+++ lib/spi/data_service.ts
@@ -2,31 +2,22 @@ import {KObject, KQueryOption, ObjectFieldMeta, ObjectMeta} from "../structs/dat
 import {FlowContext} from "./flow_plugin";
 
 interface DataService {
-
     QueryAllData(
-        ctx:FlowContext,
-        objectMeta:ObjectMeta,
-        option:KQueryOption,
-    ):KObject[]
+        ctx: FlowContext,
+        objectMeta: ObjectMeta,
+        option: KQueryOption
+    ): Promise<KObject[]>;
 
     UpdateRecord(
-        ctx:FlowContext,
+        ctx: FlowContext,
         objectID: string,
         recordID: string,
         update_fields: Record<string, any>
-    ): number
+    ): Promise<number>;
 
-    DeleteRecord(
-        ctx: FlowContext,
-        objectID: string,
-        recordID: string
-    ): number
+    DeleteRecord(ctx: FlowContext, objectID: string, recordID: string): Promise<number>;
 
-    AddRecord(
-        ctx:FlowContext,
-        object:KObject,
-    ): string
+    AddRecord(ctx: FlowContext, object: KObject): Promise<string>;
 }
-
 
 export default DataService;
